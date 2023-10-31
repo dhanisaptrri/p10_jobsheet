b941f3586cae535a62152944cc652e6ed7bdb98c
dhanisaptrri <shadhanisa2@gmail.com>
2023-10-31 20:43:31 +0700

    modifikasi

--- Numbers07.java
+++ Numbers07.java
@@ -15,6 +15,10 @@ public class Numbers07 {
         for(int i = 0; i < myNumbers.length; i++){
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+
+        for (int i = 0; i < myNumbers.length; i++){
+            System.out.println("Panjang baris ke-" + (i+1) + myNumbers[1].length);
+        }
         
     }
     
